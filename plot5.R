9f4b295d44831611f144054e32343a1f4e54e045
Ryan Brubaker <ryan.brubaker@gmail.com>
2014-06-21 22:18:13 -0500

    Added script and plot for problem 5

---
+++ plot5.R
@@ -0,0 +1,45 @@
+# Bar plot to show the total emissions for
+# motor vehicle related sources.
+
+# Motor Vehicle sources were determined by searching
+# the SCC.Level.Three field for items that contained the 
+# string "Vehicles ". I assumed that the question was 
+# concerned about on-road vehicles and not vehicles such
+# as construction/mining equipemnt Investigation of the results 
+# of this filtering showed that it provided the sources
+# asked for in the problem.
+
+# The plot shows that overall emissions from motor-vehicle related
+# sources decreased from 1999 to 2008. However, emissions
+# did rise in 2005.
+
+# This function assumes both the summary data file
+# and clasification file are in the current working directory.
+generatePlot <- function()
+{
+    # Read in both data files 
+    neiData <- readRDS("summarySCC_PM25.rds")
+    sccData <- readRDS("Source_Classification_Code.rds")
+    
+    # Pull out all motor vehicle related classifications from scc data and merge data
+    vehicleClassifications <- sccData[grepl("Vehicles ", sccData[,c("SCC.Level.Three")]), ]
+    baltimoreData <- neiData[neiData$fips == "24510", c("year", "Emissions")]
+    
+    mergedData <- merge(baltimoreData, vehicleClassifications)
+    
+    # Split by year and sum up totals
+    years <- split(mergedData$Emissions, mergedData$year)
+    emissions <- lapply(years, sum)
+    emissions <- unlist(emissions) / 1000
+    
+    png("plot5.png", width=620, height=480)
+    
+    myPlot <- barplot(emissions,
+                      col="#daf0dd",
+                      main=expression('Total Emissions of PM'[2.5] * ' from Motor Vehicle Related Sources in Baltimore City, MD'),
+                      xlab="Year",
+                      ylab="Thousands of Tons")
+    
+    dev.off()
+}
+
